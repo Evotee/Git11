708753e2499676bae472178498a7495ded885d5d
Evotee <Zdorovets2008@yandex.ru>
2023-11-17 20:03:30 +0300

    pdflj;lk

---
+++ main.py
@@ -0,0 +1,44 @@
+import sys
+import random
+
+from PyQt5.QtCore import QPointF
+from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtGui import QPainter, QColor
+
+
+class Suprematism(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 500, 500)
+        self.setWindowTitle('Суперматизм')
+
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+        self.paint2(qp)
+
+    def paint(self):
+        self.repaint()
+
+    def paint2(self, qp):
+        a = random.randint(20, 100)
+        c1 = random.randint(0, 255)
+        c2 = random.randint(0, 255)
+        c = random.randint(0, 255)
+        qp.setBrush(QColor(c, c1, c2))
+        qp.drawEllipse(QPointF(200, 200), a, a)
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Suprematism()
+    ex.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
