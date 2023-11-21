088a56e91c958d970f0e0e651682ee0dfff536fa
Evotee <Zdorovets2008@yandex.ru>
2023-11-21 19:42:20 +0300

    pdflj;lk

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from PyQt5.QtWidgets import QWidget, QApplication
 from PyQt5.QtGui import QPainter, QColor
 
 
-class Suprematism11(QWidget):
+class Suprematism111(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
@@ -38,7 +38,7 @@ def except_hook(cls, exception, traceback):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Suprematism11()
+    ex = Suprematism111()
     ex.show()
     sys.excepthook = except_hook
     print()
