b15e94c06c961f7587a9958e60612dd4fa045332
Anastasiya <Zdorovets2008@yandex.ru>
2023-11-23 23:19:49 +0300

    ,gckghc

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 import sys
 import random
 
+from PyQt5 import uic
 from PyQt5.QtCore import QPointF
 from PyQt5.QtWidgets import QWidget, QApplication
 from PyQt5.QtGui import QPainter, QColor
 
 
-class Suprematism1(QWidget):
+class Suprematism(QWidget):
     def __init__(self):
         super().__init__()
-        self.initUI()
-        pass
-
-    def initUI(self):
-        self.setGeometry(300, 300, 500, 500)
-        self.setWindowTitle('Суперматизм')
-
-    def paintEvent(self, event):
+        uic.loadUi('untitld.ui', self)
+    def paintEvent(self):
         qp = QPainter()
         qp.begin(self)
         self.paint2(qp)
@@ -26,21 +21,12 @@ class Suprematism1(QWidget):
 
     def paint2(self, qp):
         a = random.randint(20, 100)
-        c1 = random.randint(0, 255)
-        c2 = random.randint(0, 255)
-        c = random.randint(0, 255)
-        qp.setBrush(QColor(c, c1, c2))
+        qp.setBrush(QColor('yellow'))
         qp.drawEllipse(QPointF(200, 200), a, a)
 
 
-def except_hook(cls, exception, traceback):
-    sys.__excepthook__(cls, exception, traceback)
-
-
 if __name__ == '__main__':
-    app = QApplication1(sys.argv)
-    ex = Suprematism1()
+    app = QApplication(sys.argv)
+    ex = Suprematism()
     ex.show()
-    sys.excepthook = except_hook
-    print()
     sys.exit(app.exec())
